1fd20066cb3e04e613ef821e6af178a619bb3f1f
vkalia602 <vkalia602@gmail.com>
2018-03-07 05:03:28 +0000

    First Commit

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+
+	return (left + right + 1);
+}
